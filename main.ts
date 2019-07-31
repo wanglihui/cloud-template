1429e4437dd6f5bcedc32b26f0fba3f2d0e78930
wanglihui <lihui@guanghe.com>
2019-07-31 18:57:51 +0800

    init cloud template

---
+++ main.ts
@@ -0,0 +1,61 @@
+import express  = require('express');
+import registryClient, {RegistryClient} from 'cloud-registry-client';
+import {scannerDecoration, registerControllerToRouter} from 'ts-express-restful';
+import * as path from 'path';
+
+const app = express();
+const router = express.Router();
+
+scannerDecoration(path.resolve(__dirname, 'server'), [/\.js$/, /\.js\.map$/, /\.\d.ts$/]);
+registerControllerToRouter(router);
+
+app.use('/api/v1', router);
+
+import * as http from 'http';
+import * as net from 'net';
+async function main() {
+    await startServer({
+        registry: "http://localhost:5000/api/v1/app"
+    });
+}
+
+interface IServerConfig {
+    port?: number|string;
+    registry?: string | boolean;
+}
+
+async function startServer(options?: IServerConfig) {
+    let defaultOptions = {
+        port: 0,
+        registry: false,
+    }
+    options = Object.assign(defaultOptions, options) as IServerConfig;
+    const server = http.createServer(app);
+    server.on('listening', async () => {
+        let addr = (server.address() as net.AddressInfo);
+        console.log(`SERVER STARTED LISTENING ON ${addr.address}:${addr.port}...`);
+        if (options!.registry) {
+            registryClient.init({
+                url: options!.registry as string,
+            });
+
+            await registryClient.registry({
+                name: require(path.resolve(process.cwd(), 'package.json')).name,
+                ip: addr.address as string,
+                port: addr.port as number,
+            })
+        }
+    });
+    server.on('error', (err) => {
+        console.error(`server started error:`, err);
+        throw err;
+    });
+    server.listen(options.port);
+    return server;
+}
+
+main()
+.catch( (err) => {
+    console.error('系统启动失败：', err);
+    process.exit(-1);
+})
