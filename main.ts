9aede504d63f7cf900d1099297de6057d92daad7
wanglihui <lihui@guanghe.com>
2019-08-01 13:44:12 +0800

    modify logic

--- main.ts
+++ main.ts
@@ -70,6 +70,10 @@ main()
 })
 
 const death = require('death');
-death(function() {
-    registryClient.unRegistry();
+death(async () => {
+    try {
+        await registryClient.unRegistry();
+    } finally {
+        process.exit(-1);
+    }
 })
